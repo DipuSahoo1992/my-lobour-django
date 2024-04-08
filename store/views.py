995c56497031315123b11459cfd3a6263bcd96ee
phalguni <phalgunisen7@gmail.com>
2024-04-08 21:00:39 +0530

    my first commit

---
+++ store/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def hello(request):
+    return HttpResponse("<h1>hello from my django<h1>")
+
+
+def about(request):
+    return HttpResponse("<h2> about <h2>")
